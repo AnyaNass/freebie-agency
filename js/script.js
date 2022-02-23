3fd7ac815ceb64c52a789fc392b92685c77e4ccd
Анна Шаповалова <aaaananas1@gmail.com>
2022-02-23 11:53:56 +0200

    fixed, add blacl-white

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function toggleTheme(theme) {
+	const body = document.body;
+	body.classList.remove('dark', 'light')
+	if (theme == 'dark') {
+		 body.classList.add('dark')
+	} else {
+		 body.classList.add('light')
+	}
+}
+
 $('.slider_bunner').slick({
 	dots: true,
 	infinite: true,
